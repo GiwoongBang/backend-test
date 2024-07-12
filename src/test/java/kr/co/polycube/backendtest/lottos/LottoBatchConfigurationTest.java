1b250a3bf39b7b2f24f2e3c4b42b080bee8aa515
GiwoongBang <107822628+GiwoongBang@users.noreply.github.com>
2024-07-12 17:03:59 +0900

    test: LottoBatch feature Test Code

---
+++ src/test/java/kr/co/polycube/backendtest/lottos/LottoBatchConfigurationTest.java
@@ -0,0 +1,76 @@
+package kr.co.polycube.backendtest.lottos;
+
+import kr.co.polycube.backendtest.BackendTestApplication;
+import kr.co.polycube.backendtest.lottos.batch.LottoBatchConfiguration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBatchTest
+@SpringJUnitConfig(classes = LottoBatchConfiguration.class)
+@SpringBootTest(classes = BackendTestApplication.class)
+public class LottoBatchConfigurationTest {
+
+    @Autowired
+    private JobLauncher jobLauncher;
+
+    @Autowired
+    private Job lottoJob;
+
+    @Autowired
+    private LottoRepository lottoRepository;
+
+    @Autowired
+    private WinnerRepository winnerRepository;
+
+    @Autowired
+    private WinnerService winnerService;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        lottoRepository.deleteAll();
+        winnerRepository.deleteAll();
+
+        Set<Integer> lottoNumbers1 = new HashSet<>(Set.of(1, 2, 3, 4, 5, 6));
+        Set<Integer> lottoNumbers2 = new HashSet<>(Set.of(3, 4, 5, 6, 7, 8));
+        Set<Integer> lottoNumbers3 = new HashSet<>(Set.of(7, 8, 9, 10, 11, 12));
+        lottoRepository.save(new LottoEntity(lottoNumbers1));
+        lottoRepository.save(new LottoEntity(lottoNumbers2));
+        lottoRepository.save(new LottoEntity(lottoNumbers3));
+
+        String winningNumbers = "1 2 3 4 5 6";
+        winnerService.setWinningNumbers(winningNumbers);
+    }
+
+    @Test
+    void 로또_당첨_여부_배치_처리() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+        JobExecution jobExecution = jobLauncher.run(
+                lottoJob,
+                new JobParametersBuilder()
+                        .addLong("time", System.currentTimeMillis())
+                        .toJobParameters()
+        );
+
+        assertThat(jobExecution.getExitStatus().getExitCode()).isEqualTo("COMPLETED");
+        assertThat(winnerRepository.findAll()).hasSize(2);
+
+        WinnerEntity winner = winnerRepository.findAll().get(0);
+        assertThat(winner.getRank()).isEqualTo(1);
+    }
+}
